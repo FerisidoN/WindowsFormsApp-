ce95e8d0067e856c2a6daae9822385c853678f34
oleg4 <oleg4@LAPTOP-MSQS2C0F>
2022-10-19 18:37:27 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp Курсор/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Курсор
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+        }
+       
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Random r = new Random();
+            button1.Left = r.Next(0, this.Size.Width - button1.Width);
+            button1.Top = r.Next(0, this.Size.Height - button1.Height);
+        }
+    }
+}
